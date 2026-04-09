59204cad613a9ee56924fb278413434995e3fe55
Oumayma-Outh <outhmanioumayma32@gmail.com>
2026-04-09 16:01:49 +0200

    Patient Screen

--- lib/screens/PatientScreen.dart
+++ lib/screens/PatientScreen.dart
@@ -1,1 +1,14 @@
 
+class PatientScreen extends StatelessWidget {
+  const PatientScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Paciente')),
+      body: const Center(
+        child: Text('Pantalla del paciente'),
+      ),
+    );
+  }
+}
