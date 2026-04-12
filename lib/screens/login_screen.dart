0e6b2fa704eebff4e1d844001bc9a3e16a516764
Oumayma-Outh <outhmanioumayma32@gmail.com>
2026-04-12 17:15:27 +0200

    refactor : Redesign Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:emm_app/components/my_button.dart';
 import 'package:emm_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
-import '../components/my_textfield.dart';
 import '../widgets/logoEmm_widget.dart';
 
 
@@ -13,11 +12,10 @@ class LoginScreen extends StatelessWidget {
 
   
   @override
- Widget build(BuildContext context){
-  return Scaffold(
-
-    backgroundColor: const Color(0xFFF7F3FA),
-   body: SafeArea(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F3FA),
+      body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
             child: Padding(
@@ -26,11 +24,11 @@ class LoginScreen extends StatelessWidget {
                 children: [
                   const SizedBox(height: 20),
 
-                  // LOGO 
+                  // LOGO
                   Container(
                     width: 90,
                     height: 90,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Colors.white,
                       shape: BoxShape.circle,
                       boxShadow: [
@@ -41,66 +39,78 @@ class LoginScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(18),
+                    child:  Padding(
+                      padding: EdgeInsets.all(18),
                       child: logoWidget(),
                     ),
                   ),
 
                   const SizedBox(height: 30),
 
-                  const Text(
-                    'EMM_Login',
-                    style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF1D2A3A),
-                    ),
+
+                const Text(
+                  'EMM_Login',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF1D2A3A),
                   ),
-
-          const   SizedBox(height: 50),
-
-             const Text(
-                    'Inicia sesión para continuar',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Color(0xFF4A5568),
-                    ),
-                  ),
-
-             const     SizedBox(height: 50),
-
-               //USERNAME:
-                MyTextfield(
-                  controller:usernameController,
-                  hintText: 'Username',
-                  obscureText: false,
-
-
                 ),
 
-            const    SizedBox(height: 20),
+                const SizedBox(height: 10),
 
-                //password:
-                MyTextfield(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
+                const Text(
+                  'Inicia sesión para continuar',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xFF4A5568),
+                  ),
                 ),
 
-             const    SizedBox(height: 30),
+                const SizedBox(height: 30),
 
-             //button:
 
-             MyButton(),
-              const SizedBox(height: 20),
-        ],
+                  // card de login 
+                  Container(
+                    padding: const EdgeInsets.all(25),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: SizedBox(
+                      width: 300,
+                      child: Column(
+                        children: [
+                          MyTextfield(
+                            controller: usernameController,
+                            hintText: 'Username',
+                            obscureText: false,
+                          ),
+                          const SizedBox(height: 20),
+                          MyTextfield(
+                            controller: passwordController,
+                            hintText: 'Password',
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 30),
+                          MyButton(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-      ),
-      ),
-      ),
-      ),
-  );
- }
-  
+    );
+  }
 }
